70ccc91f3578746850ed8dc4d850bdf06f5fd146
Biljana Kotevska <biljanakotev@gmail.com>
2023-07-26 13:29:03 +0200

    finished iterations 1-7

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,41 +1,69 @@
-const express = require('express')
-const router = express.Router()
-const axios = require('axios')
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-	res.render('index')
-})
+  res.render('index');
+});
 
 //ITERATION 3
 //GET route for searching for 1 character
 router.get('/all', (req, res, next) => {
-	//Write code here
-	
-})
+  axios
+    .get('http://localhost:8000/characters')
+    .then((results) => {
+      console.log(results.data);
+      res.render('index', { allCharacters: results.data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //ITERATION 4
 //GET route for searching for 1 character
 router.get('/search', (req, res, next) => {
-	//Write code here
-})
+  const { id } = req.query;
+  axios
+    .get(`http://localhost:8000/characters/${id}`)
+    .then((results) => {
+      res.render('index', { oneCharacter: results.data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //ITERATION 5
 //POST route for deleting 1 character
 router.post('/search/delete', (req, res, next) => {
-	//Write code here
-})
+  const id = req.body.id;
+  axios
+    .delete(`http://localhost:8000/characters/${id}`)
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //ITERATION 6
 //POST route for creating a new character
 router.post('/search/new-character', (req, res, next) => {
-	//Write code here
-})
+  const { name, occupation, cartoon, weapon } = req.body;
+  axios.post(`http://localhost:8000/characters/`, { name, occupation, cartoon, weapon }).then(() => {
+    res.redirect('/');
+  });
+});
 
 //ITERATION 7
 //POST route for editing 1 character
 router.post('/search/edit', (req, res, next) => {
-	//Write code here
-})
+  axios.put(`http://localhost:8000/characters/${req.body.id}`, req.body).then(() => {
+    res.redirect('/');
+  });
+});
 
-module.exports = router
+module.exports = router;
